3ca2ace98336f131ff2383b1d712b870bad9e624
Jon Badgett <jonthenerd@users.noreply.github.com>
2023-01-08 13:04:21 -0600

    allow theme to persist past navigation

--- static/assets/scripts/main.js
+++ static/assets/scripts/main.js
@@ -2,10 +2,11 @@
     const themeToggle = document.getElementById("theme-toggle");
     const lightStyles = document.querySelectorAll("link[rel='stylesheet'][media='(prefers-color-scheme: light)']");
     const darkStyles = document.querySelectorAll("link[rel='stylesheet'][media='(prefers-color-scheme: dark)']");
+    let forced = false;
     let isDark;
 
     function updateText() {
-        themeToggle.innerText = `${(isDark) ? "Dark" : "Light"} Mode`;
+        themeToggle.innerText = isDark ? "Dark" : "Light";
     }
     function disableStyles(styles) {
         styles.forEach(style => {
@@ -19,14 +20,7 @@
             style.setAttribute("media", "all");
         });
     }
-
-    // Set Initial Text
-    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    isDark = darkModeMediaQuery.matches;
-    updateText();
-
-    // Respond to Toggle Clicks
-    themeToggle.addEventListener("click", () => {
+    function toggleTheme() {
         if (isDark) {
             disableStyles(darkStyles);
             enableStyles(lightStyles);
@@ -35,6 +29,34 @@
             enableStyles(darkStyles);
         }
         isDark = !isDark;
+        try {
+            window.localStorage.setItem("mode", isDark ? "dark" : "light");
+            forced = true;
+        } catch { }
         updateText();
+    }
+
+    // Get Browser Preference
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    isDark = darkModeMediaQuery.matches;
+
+    // Change theme if previously clicked
+    try {
+        const stored = window.localStorage.getItem("mode");
+        if (stored) {
+            forced = true;
+
+            if (!((stored === "dark" && isDark) || (stored === "light" && !isDark))) {
+                toggleTheme();
+            }
+        }
+
+    } catch { }
+
+    updateText();
+
+    // Respond to Toggle Clicks
+    themeToggle.addEventListener("click", () => {
+       toggleTheme();
     })
 })();
